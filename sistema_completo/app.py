713f7a72611c19dba79d257b26acc75001f3fac5
ViniciusTeixeira-Dev <contato.viniciusaraujoteixeira@gmail.com>
2025-03-19 21:48:25 +0000

    feat: diretorios

---
+++ sistema_completo/app.py
@@ -0,0 +1,13 @@
+import sqlite3
+from flask import Flask, flash, redirect, render_template, request, session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# Configurar o Flask
+app = Flask(__name__)
+
+# Banco de Dados
+connect = sqlite3.connect("MUDAR AINDA")
+db = connect.cursor()
+
+
+
