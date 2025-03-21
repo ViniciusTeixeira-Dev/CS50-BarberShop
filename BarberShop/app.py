93edf0915e9dc3d19000670ad72a0e269f705193
ViniciusTeixeira-Dev <contato.viniciusaraujoteixeira@gmail.com>
2025-03-21 23:34:14 +0000

    add helper.py

--- BarberShop/app.py
+++ BarberShop/app.py
@@ -14,6 +14,13 @@ app = Flask(__name__)
 def index():
     return render_template("layout.html")
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True)
 
